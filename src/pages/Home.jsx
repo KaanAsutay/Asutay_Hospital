e028e272f2c506ebf03f7874aa4084fd57b5d23c
Kaan <kaanasutay10@gmail.com>
2023-12-08 19:55:23 +0300

    finish

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import AppointmentList from "../components/AppointmentList"
+import Doctors from "../components/Doctors"
+import { appointmentData } from "../helper/data"
+import { useState } from "react"
+
+const Home = () => {
+  const [appointments, setAppointments] = useState(appointmentData)
+
+  console.log(appointments)
+  return (
+    <main className="text-center mt-2">
+      <h1 className="display-5 text-danger">ASUTAY HOSPITAL</h1>
+      <Doctors apps={appointments} setApps={setAppointments} />
+      <AppointmentList apps={appointments} setApps={setAppointments} />
+    </main>
+  )
+}
+
+export default Home
